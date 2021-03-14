01cb08657af96a8181fad745c47bfff5e4615256
Muhammad Bagus Gunawan <65713531+E31190237@users.noreply.github.com>
2021-03-14 23:16:11 +0700

    Create Tugas 1.php

---
+++ Minggu 1/Tugas 1.php
@@ -0,0 +1,20 @@
+<?php 
+
+class buku {
+
+     public $judulBuku;
+     public $pengarang;
+     public $penerbit;
+     public $tahunTerbit;
+     public $catakan;  
+}
+?>
+
+<!-- 
+class buku {
+     public $judulBuku='Ronggeng Dukuh Paruk';
+     public $pengarang='Ahmad Tohari';
+     public $penerbit='Gramedia Pustaka Utama';
+     public $tahunTerbit='1982';
+     public $catakan='pertama';  
+}
