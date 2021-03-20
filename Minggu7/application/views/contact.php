29b60b118e1e26fdd8cf8afdb2e6e590e21ae253
Muhammad Bagus Gunawan <65713531+E31190237@users.noreply.github.com>
2021-03-20 16:19:15 +0700

    apdet Minggu 7

---
+++ Minggu7/application/views/contact.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    <h1>iki contact</h1>
+    <p>Okaerinasai (お帰りなさい),ini adalah halaman contact </p>
+</body>
+</html>
