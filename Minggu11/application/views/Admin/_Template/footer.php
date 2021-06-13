54377c00a258ba30426f24df5d38fb2c0b83ed62
Muhammad Bagus Gunawan <65713531+E31190237@users.noreply.github.com>
2021-06-13 12:56:55 +0700

    Apdet Week 11 Ya Gaes Ya

---
+++ Minggu11/application/views/Admin/_Template/footer.php
@@ -0,0 +1,8 @@
+  <!-- Footer -->
+              
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>KopiKanan &copy; Gun Was Here 2021</span>
+                    </div>
+                </div>
+            <!-- End of Footer -->
